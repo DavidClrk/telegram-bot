7e3df21dc5b39f2f1c18b7d636e6dd143b52056f
DavidClrk <muizzabdulla@gmail.com>
2017-01-12 16:53:05 +0500

    Update StatbBot.php

--- StatbBot.php
+++ StatbBot.php
@@ -1,1 +1,13 @@
 
+<?php
+
+$botToken = "300006676:AAEOGtGg7fETHQqr0Ojppl6NuggGmB_IXUg";
+$website = "https://api.telegram.org/bot".$botToken;
+
+$update = file_get_contents($website"/getupdates");
+
+$updateArray = jason_decode($update, TRUE);
+
+print_r($updateArray);
+
+?>
